b00d0b41ef1a709b38bbea0d5f47aec9c95cad23
Programador Novato <tonicintora@gmail.com>
2021-10-20 20:26:24 +0200

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    // Primary target to follow, to get his position.
     [SerializeField] private Transform target;
+    // Quantity to offset the position of the camera before moving it
     [SerializeField] private Vector3 offset;
+    // Time to correct the offset to the right camera position
     [SerializeField] private float smoothSpeed = 0.125f;
     private Vector3 velocity = Vector3.zero;
     
     private void LateUpdate() {
+        // Defines the desired amount of space between the real position and the desired position
         Vector3 desiredPositon = target.position + offset;
+        // Transform the position using the SmoothDamp() function to get a smooth effect based on the parameters given.
         transform.position = Vector3.SmoothDamp(transform.position, desiredPositon, ref velocity, smoothSpeed);
     }
 }
